4870e6697a64a4539dffaa2f510726e67107fb84
Milena Siqueira <milematoss1@gmail.com>
2023-04-18 22:43:10 -0300

    feat: first inputs draft

--- pages/avaliar-projeto.vue
+++ pages/avaliar-projeto.vue
@@ -1,23 +1,60 @@
 <template>
-  <cv-progress
-  :initial-step="initialStep"
-  :steps="steps"></cv-progress>
+  <div class="avaliar-projeto">
+    <h3>Avalie seu projeto de acordo com os indicadores FORPROEX e Objetivos de Desenvolvimento Sustentável!</h3>
+    
+    <p>Preencha as informações no formulário abaixo para realizar sua avaliação. Seus dados não serão salvos nem compartilhados.</p>
+
+    <cv-progress
+    class="stepper"
+    :initial-step="currentStepNumber"
+    :steps="steps" />
+
+    <div class="form">
+      <component :is="item.name" v-for="item in currentStep" v-bind="item"/>
+    </div>
+  </div>
 </template>
 
 <script>
+import formSteps from '~/assets/form-steps.json'
+
 export default {
   name: 'AvaliarProjetoPage',
   data() {
     return {
-      initialStep: 2,
-      steps: [
-        "Step 1",
-        "Step 2 overflows and shows a tip",
-        "Step 3",
-        "Step 4",
-        "Step 5"
-      ],
+      currentStepNumber: 0,
+      steps: formSteps.steps,
+    }
+  },
+  computed: {
+    currentStep() {
+      return formSteps[this.currentStepNumber]
     }
   }
 }
 </script>
+
+<style lang="scss">
+@import "../styles/carbon";
+.avaliar-projeto {
+  display: grid;
+  margin-top: 16px;
+  text-align: unset;
+
+  h3, p {
+    text-align: center;
+  }
+}
+
+.stepper {
+  justify-content: center;
+}
+
+.bx--progress-step, .bx--progress-line {
+  width: 15rem !important;
+}
+
+.bx--progress-label {
+  max-width: 15rem !important;
+}
+</style>
