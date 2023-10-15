3bc395b82ab9d2882f606b2084ecf05ae3e195f2
Milena Siqueira <milematoss1@gmail.com>
2023-10-15 18:29:56 -0300

    fix: page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'apex',
+    title: 'Analisador PEX',
     htmlAttrs: {
       lang: 'en'
     },
