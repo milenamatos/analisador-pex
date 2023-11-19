07bb727cefb56360b982922c9f10f111c71f0ab2
Milena Siqueira <milematoss1@gmail.com>
2023-11-18 22:11:01 -0300

    config: get env

--- assets/services/apex-api.js
+++ assets/services/apex-api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const apiHost = process.env.APEX_API || 'http://localhost:3001'
+export const apiHost = process.env.APEX_API || 'https://analisador-pex-api-1c47044f34e9.herokuapp.com/'
+console.log(process.env.APEX_API)
 
 const axiosClient = axios.create({
   baseURL: apiHost,
