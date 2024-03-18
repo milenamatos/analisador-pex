9e6f8536f6bdfd0091aecd5e93d10fea689ede76
Milena Siqueira <milematoss1@gmail.com>
2024-03-18 20:29:48 -0300

    chore: fix env

--- assets/services/apex-api.js
+++ assets/services/apex-api.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-export const apiHost = process.env.APEX_API || 'http://localhost:3001/'
-console.log(process.env.APEX_API)
+export const apiHost = process.env.NUXT_ENV_APEX_API || 'http://localhost:3001/'
 
 const axiosClient = axios.create({
   baseURL: apiHost,
