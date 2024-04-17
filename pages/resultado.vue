b1cd4650fb3c417dc060365f6273b0e90558e030
Milena Siqueira <milematoss1@gmail.com>
2024-04-16 21:09:09 -0300

    feat: include more details

--- pages/resultado.vue
+++ pages/resultado.vue
@@ -27,28 +27,44 @@
 
     <cv-content-switcher-content owner-id="relatorio">
       <section ref="pdf-content">
-        <h3 class="title">Resultado da análise</h3>
-
         <cv-grid fullWidth class="result">
           <p class="left-align">
             A análise apresentada neste relatório foi realizada em {{ date }} às {{ time }}, através do site <cv-link :href="host" target="_blank">{{ host }}</cv-link>
           </p>
 
-          <cv-tile fullWidth class="details left-align">
+          <cv-tile fullWidth class="details text-justify">
             <h4>Sobre a ferramenta:</h4> 
             
             <p>
               O objetivo do Analisador PEX (Projetos de Extensão) é auxiliar na visualização de informações úteis para a autoavaliação dos projetos de extensão universitária. Para isto, a ferramenta realiza uma análise por meio de cruzamentos entre indicadores de extensão e ODS (Objetivos de Desenvolvimento Sustentável), criando uma maneira alternativa de se avaliar os projetos. 
             </p>
             <p>
-              Os indicadores utilizados são os Indicadores Brasileiros de Extensão Universitária (IBEU) estabelecidos pelo FORPROEX e utilizados no ramo acadêmico para medir e avaliar os projetos de extensão. Para mais detalhes, veja o Relatório de Pesquisa oficial. Já os ODS são todos os 17 objetivos definidos pela ONU. Para mais detalhes visite o <cv-link href="https://brasil.un.org/pt-br/sdgs" target="_blank">site oficial da ONU no Brasil</cv-link>.
+              Os indicadores utilizados são os 52 Indicadores Brasileiros de Extensão Universitária (IBEU) estabelecidos pelo FORPROEX e utilizados no ramo acadêmico para medir e avaliar os projetos de extensão. Para mais detalhes, veja o Relatório de Pesquisa oficial. Já os ODS são todos os 17 objetivos definidos pela ONU. Para mais detalhes visite o <cv-link href="https://brasil.un.org/pt-br/sdgs" target="_blank">site oficial da ONU no Brasil</cv-link>.
             </p>
             <p>
               Esta ferramenta foi desenvolvida como trabalho de conclusão de curso (TCC) para o curso de Ciência da Computação na UNIFESP. A metodologia utilizada para implementação desta ferramenta foi apresentada no trabalho de Mestrado Profissional de Inovação Tecnológica "Ferramenta para análise de projetos de Extensão Universitária sob a ótica da relação entre os Indicadores de Extensão e os ODS". 
             </p>    
+          </cv-tile>
 
+          <cv-tile fullWidth class="details text-justify">
+            <h4>Resumo da metodologia:</h4> 
+
+            <p>
+              A metodologia utiliza métodos de gestão estratégica e sustentável estruturados pelo modelo <em>Balanced Scorecard</em> (BSC) aliado ao modelo <em>Triple Bottom Line (TBL)</em>. A partir desses modelos, apresenta-se uma ferramenta que possibilita avaliar preliminarmente o tipo de relação existente entre os projetos e programas de Extensão Universitária (com base em suas características e seleção de indicadores FORPROEX) e os ODS por meio de gráficos e tabelas. A união de metodologias como o BSC e o TBL tem o objetivo de atender à crescente demanda por estratégias que combinem meio ambiente, sociedade e questões éticas com formas de medir o desempenho sustentável no ambiente corporativo utilizando estratégias de avaliação que combinam mais de um <em>framework</em>. 
+            </p>
+            <p>              
+              O mapa estratégico sobre pesquisa universitária utilizado pelo FORPROEX incorporou a metodologia empregada pelo modelo BSC utilizando os 52 indicadores da avaliação da Extensão Universitária e adaptando os quatro quadrantes tradicionais do modelo para quadrantes que representam as seguintes perspectivas: <strong>Do Aluno, da Sociedade e dos Financiadores Públicos; Dos Processos Internos; Da Aprendizagem e Crescimento; Dos Recursos Financeiros e Infraestrutura </strong>. E para incorporação do método TBL, a autora do mestrado na qual esta ferramenta se baseia, distribuiu os ODS nos pilares definidos pelo mesmo, são eles: <strong>Sociocultural, Meio Ambiente e Econômico.</strong>
+            </p>
+            <p>
+              Desta forma foi definida a metodologia híbrida de cruzamento entre o BSC e o TBL: uma matriz de cruzamento 52 x 17, denominada Matriz de Referência, apresenta os Indicadores de Extensão Universitária do FORPROEX nas linhas, distribuídos de acordo com os quadrantes do BSC, e nas colunas os ODS distribuídos dentro dos pilares de sustentabilidade do modelo TBL. Com base nessa Matriz são realizados cruzamentos e análises para aferir a aderência de um programa ou projeto de extensão aos ODS com base nos dados informados (informações de entrada) para o projeto analisado.
+            </p>
+          </cv-tile>
+
+          <cv-tile fullWidth class="left-align">
             <FormReview :filter="false" label="formattedName" class="left-align" />
           </cv-tile>
+
+          <h3 class="title">Resultado da análise</h3>
 
           <cv-row v-for="chart in charts" :key="chart">
             <cv-column>
@@ -234,6 +250,10 @@ export default {
   text-align: left;
 }
 
+.text-justify {
+  text-align: justify;
+}
+
 .details {
   display: grid;
   row-gap: 16px;
@@ -242,7 +262,7 @@ export default {
 
 .result {
   display: grid;
-  grid-gap: 70px;
+  grid-gap: 48px;
   margin-top: 40px;
   padding: 0;
   width: 100%;
