7abdded067c04f27017e4f210fb4a7d4ae385394
Milena Siqueira <milematoss1@gmail.com>
2024-06-24 22:15:54 -0300

    feat: update menu nav

--- components/header-nav.vue
+++ components/header-nav.vue
@@ -8,9 +8,19 @@
       <cv-header-menu-item class="header-menu__item" href="/analisar-projeto">
         Faça uma análise!
       </cv-header-menu-item>
-      <cv-header-menu-item class="header-menu__item" href="/sobre">
-        Sobre o projeto
-      </cv-header-menu-item>
+      
+      <cv-header-menu aria-label="Conteúdo" title="Conteúdo">
+        <cv-header-menu-item class="header-menu__item" href="/sobre">
+          Sobre o projeto
+        </cv-header-menu-item>
+        <cv-header-menu-item class="header-menu__item" href="/metodologia">
+          Metodologia
+        </cv-header-menu-item>
+        <cv-header-menu-item class="header-menu__item" href="/ibeu">
+          IBEU
+        </cv-header-menu-item>
+      </cv-header-menu>
+      
       <cv-header-menu-item class="header-menu__item" href="/tutorial">
         Tutorial
       </cv-header-menu-item>
@@ -26,15 +36,15 @@ export default {
 
 <style lang="scss">
 @import "../styles/carbon";
-.header-menu {
+.header-menu, .bx--header__menu-item {
   background-color: $link-02 !important;
   
   a {
     color: white !important;
+  }
 
-    &:hover {
-      background-color: $hover-primary !important;
-    }
+  a:hover, a.bx--header__menu-item:active, .bx--header__action:active {
+    background-color: $hover-primary !important;
   }
 }
 </style>
